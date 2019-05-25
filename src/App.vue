db5b91b6a7b0047e8c94d76248de67335679349a
kkterai <kkterai@gmail.com>
2019-05-25 16:44:01 -0700

    Update dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SignIn msg="Welcome to WeMix 'Vue-point-0' "/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignIn from './components/SignIn.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SignIn
   }
 }
 </script>
